295471a87cf81006cc79ce439eeb685ceda0055d
nigel <nigel.j.tye@gmail.com>
2023-03-21 20:49:47 +1100

    Please Fix

---
+++ src/config/Config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyB16VLL9L03jYQQFbxIruevqQPovmGBhfg",
+  authDomain: "classproject-61e37.firebaseapp.com",
+  projectId: "classproject-61e37",
+  storageBucket: "classproject-61e37.appspot.com",
+  messagingSenderId: "654100236698",
+  appId: "1:654100236698:web:2e047fd2261e32dce05223",
+  measurementId: "G-XMP2VDJGFB"
+};
