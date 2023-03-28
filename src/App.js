b15809db6ce66ac4fc2aa4829330166420441b6f
nigel <nigel.j.tye@gmail.com>
2023-03-28 20:51:59 +1100

    Added new route for different pages

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import './App.css';
 import { firebaseConfig } from './config/Config';
 import { initializeApp } from 'firebase/app';
-import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
 import { Test } from './components/Test';
+import { Home } from './pages/Home';
+import { About } from './pages/About';
+import { Contact } from './pages/Contact';
+import { Routes, Route } from 'react-router-dom';
 
 const firebaseApp = initializeApp(firebaseConfig)
 
 function App() {
   return (
     <div className="App">
-      <Test />
-      <Test />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="About" element={<About />} />
+        <Route path="Contact" element={<Contact />} />
+      </Routes>
     </div>
   );
 }
